d4e83d9084f912c6323019c176a9e4773919254a
kpms-rizwan-mushtaq <141311644+kpms-rizwan-mushtaq@users.noreply.github.com>
2023-12-10 20:17:15 +0100

    adding docker and docker compose file

--- mysqlConfig.ts
+++ mysqlConfig.ts
@@ -1,6 +1,7 @@
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 import { User } from './src/users/entities/user.entity';
 import { Todo } from './src/todos/entities/todo.entity';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 //database settings
 // const mysqlConfig: MysqlConnectionOptions = {
@@ -18,17 +19,26 @@ import { Todo } from './src/todos/entities/todo.entity';
 
 //cleardb url = mysql://bf972b71490aa6:c95ace94@eu-cdbr-west-03.cleardb.net/heroku_82da633c91a8c1f?reconnect=true
 
-const mysqlConfig: MysqlConnectionOptions = {
-  type: 'mysql',
-  host: 'eu-cdbr-west-03.cleardb.net',
-  // port: 8000,
-  username: 'bf972b71490aa6',
-  password: 'c95ace94',
-  database: 'heroku_82da633c91a8c1f',
+const mysqlConfig: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: [User, Todo],
-  // entities: ['dist/src/**/*.entity.js'],
-  // entities: [__dirname + '/../**/*.entity{.ts,.js'],
   synchronize: true,
 };
+
+// const mysqlConfig: MysqlConnectionOptions = {
+//   type: 'mysql',
+//   host: 'eu-cdbr-west-03.cleardb.net',
+//   // port: 8000,
+//   username: 'bf972b71490aa6',
+//   password: 'c95ace94',
+//   database: 'heroku_82da633c91a8c1f',
+//   entities: [User, Todo],
+//   synchronize: true,
+// };
 
 export default mysqlConfig;
